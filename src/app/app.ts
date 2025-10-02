b661b5a8bcb2151825f36a883181571558d1cc1b
Waldecir Júnior <waldeciroajunior@hotmail.com>
2025-10-01 22:03:20 -0300

    chore: atualizações de build e pacotes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginTemplate } from './pages/auth/login-template/login-template';
+import { CadastroCliente } from './pages/cliente/cadastro-cliente/cadastro-cliente';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginTemplate],
-  template: `<app-login-template></app-login-template>`,
+  imports: [RouterOutlet, LoginTemplate, CadastroCliente],
+  templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
 export class App {
